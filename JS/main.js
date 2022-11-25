6731b131651fd03b6ca13ebabf5eb511fb5c0b08
FilippoBid <FilippoPerrone1997@gmail.com>
2022-11-25 16:45:18 +0100

    inizio con la creazione del layout html

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+import usersListmported from "./usersList";
+const { createApp } = Vue;
+
+
+createApp({
+  
+  data () {
+    
+    return {
+      usersList : usersListmported
+
+      
+    };
+  },
+  methods:{
+    
+  }
+}).mount("#app")
+
